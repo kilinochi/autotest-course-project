a23079eb8a17c8124a816534916df914022f420f
arman-sh <rosko1245@gmail.com>
2019-12-15 21:00:21 +0300

    checks

--- src/main/java/com/kilinochi/page/music/PlaylistPage.java
+++ src/main/java/com/kilinochi/page/music/PlaylistPage.java
@@ -2,11 +2,13 @@ package com.kilinochi.page.music;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import com.kilinochi.page.Page;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static org.junit.Assert.assertEquals;
 
 public final class PlaylistPage implements Page {
 
@@ -15,12 +17,18 @@ public final class PlaylistPage implements Page {
     private static final By TRACKS_LOCATOR = By.xpath("//*[@class ='wm-track_hld __3xfx9j']");
     private static final By CREATE_ALBUM_BUTTON_SELECTOR = By.xpath("//*[@data-l ='t,submit']");
 
-    public PlaylistPage() {
+    private final SelenideElement inputName;
+    private final SelenideElement createAlbumButton;
 
+
+    public PlaylistPage() {
+        inputName = $(INPUT_NAME_PLACEHOLDER_SELECTOR);
+        createAlbumButton = $(CREATE_ALBUM_BUTTON_SELECTOR);
+        check();
     }
 
     public PlaylistPage inputAlbumName(final String name) {
-        $(INPUT_NAME_PLACEHOLDER_SELECTOR).setValue(name);
+        inputName.setValue(name);
         return this;
     }
 
@@ -30,12 +38,14 @@ public final class PlaylistPage implements Page {
                 .stream()
                 .limit(firstNCount)
                 .forEach(selenideElement -> selenideElement.waitUntil(Condition.visible, 1000).scrollTo().click());
-        $(CREATE_ALBUM_BUTTON_SELECTOR).shouldHave(Condition.text("Создать")).click();
+        createAlbumButton.shouldHave(Condition.text("Создать")).click();
         return this;
     }
 
     @Override
     public Page check() {
+        String attr = inputName.getAttribute("placeholder");
+        assertEquals("Название сборника", attr);
         return this;
     }
 }
